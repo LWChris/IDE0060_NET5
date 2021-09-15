762dd94f3a02ae25120a8a956b621fd01432eb51
LWChris <LWChris@GitHub>
2021-09-16 01:29:18 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDE0060_NET5
+{
+    class Program
+    {
+        static void Main() {
+        }
+
+        public int Foo(int bar) => throw new NotImplementedException();
+    }
+}
